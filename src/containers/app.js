c596f318773eefa157f58161308106126dad797f
Marat Akhmetov <akhmetovdev@gmail.com>
2017-05-31 01:15:39 +0600

    updated project structure, added redux, router, styled-components and firebase

---
+++ src/containers/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './app.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>Vupli Comics</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import App from './containers/app';
+
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+if (navigator.serviceWorker) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`);
+  });
+}
