925bee1666be45130cb52728250d5d669f28b6e9
Marat Akhmetov <akhmetovdev@gmail.com>
2017-05-31 22:43:13 +0600

    added firebase init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase';
 
 import App from './containers/app';
 
@@ -18,3 +19,10 @@ if (navigator.serviceWorker) {
     navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`);
   });
 }
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDMTwj6sDEgD1iONRguVF8A26phw-69hkw',
+  authDomain: 'https://vupli-comics.firebaseapp.com',
+  databaseURL: 'https://vupli-comics.firebaseio.com',
+  storageBucket: 'gs://vupli-comics.appspot.com'
+});
